2f82285048e430740cd2fa1964c591c141f710d5
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-06-08 19:59:35 +0530

    dockerise the whole application

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Cohort!");
